c332890102f0bb18c76a1f4cd5bef7b4be740240
Dnice12 <emanuels.devine@gmail.com>
2021-10-19 13:49:40 +0100

    update

--- app211-enrollments/Module.java
+++ app211-enrollments/Module.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Module here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Devine Emanuels
+ * @version (19/10/2021)
  */
 public class Module
 {
