562415e908ef7ad59a7051aaccc8a32be064483d
Dnice12 <emanuels.devine@gmail.com>
2021-11-09 15:17:27 +0000

    update

--- app213-products/StockDemo.java
+++ app213-products/StockDemo.java
@@ -23,16 +23,16 @@ public class StockDemo
         // Add at least 10 products, they must be unique to you
         // Make sure the ids are sequential numbers
         
-        stock.add(new Product(101, "Mercedes AMG GT"));
+        stock.add(new Product(101, "Audi TT"));
         stock.add(new Product(102, "BMW M8 GC"));
         stock.add(new Product(103, "Audi RS3"));
         stock.add(new Product(104, "Mercedes AMG GT"));
-        stock.add(new Product(105, "BMW M8 GC"));
-        stock.add(new Product(106, "Audi RS3"));
-        stock.add(new Product(107, "Mercedes AMG GT"));
-        stock.add(new Product(108, "BMW M8 GC"));
-        stock.add(new Product(109, "Audi RS3"));
-        stock.add(new Product(110, "Audi RS3"));
+        stock.add(new Product(105, "Range Rover Evoque"));
+        stock.add(new Product(106, "Mercedes A class"));
+        stock.add(new Product(107, "Audi A8"));
+        stock.add(new Product(108, "Vauxhall Astra Griffin"));
+        stock.add(new Product(109, "Peugeot E-208"));
+        stock.add(new Product(110, "BMW 220i"));
         
     }
     
@@ -58,16 +58,16 @@ public class StockDemo
     
     private void buyProducts()
     {
-        stock.buyProduct(101,8);
-        stock.buyProduct(102,6);
-        stock.buyProduct(103,4);
-        stock.buyProduct(104,8);
-        stock.buyProduct(105,6);
-        stock.buyProduct(106,4);
-        stock.buyProduct(107,8);
-        stock.buyProduct(108,6);
-        stock.buyProduct(109,4);
-        stock.buyProduct(110,5);
+        stock.buyProduct(101,20);
+        stock.buyProduct(102,16);
+        stock.buyProduct(103,10);
+        stock.buyProduct(104,24);
+        stock.buyProduct(105,26);
+        stock.buyProduct(106,26);
+        stock.buyProduct(107,12);
+        stock.buyProduct(108,18);
+        stock.buyProduct(109,22);
+        stock.buyProduct(110,30);
     }
 
     private void sellProducts()
@@ -75,13 +75,13 @@ public class StockDemo
         stock.sellProduct(101,2);
         stock.sellProduct(102,4);
         stock.sellProduct(103,6);
-        stock.sellProduct(101,2);
-        stock.sellProduct(102,4);
-        stock.sellProduct(103,6);
-        stock.sellProduct(101,2);
-        stock.sellProduct(102,4);
-        stock.sellProduct(103,6);
-        stock.sellProduct(101,2);
+        stock.sellProduct(104,6);
+        stock.sellProduct(105,8);
+        stock.sellProduct(106,6);
+        stock.sellProduct(107,12);
+        stock.sellProduct(108,10);
+        stock.sellProduct(109,12);
+        stock.sellProduct(110,8);
     
         
     }    
